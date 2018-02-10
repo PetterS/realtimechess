7ac882b02b8753f691ea4baa5dd99b5945822e08
Petter Strandmark <petter.strandmark@gmail.com>
2018-02-10 10:42:25 +0100

    TestGame.test_move_moving passes.

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 import aiohttp
 
 
-def HttpCodeException(code, text=""):
-	return aiohttp.web.Response(status=code, text=text)
+class HttpCodeException(aiohttp.web.HTTPException):
+	def __init__(self, code, *args):
+		self.status_code = code
+		aiohttp.web.HTTPException.__init__(self, *args)
